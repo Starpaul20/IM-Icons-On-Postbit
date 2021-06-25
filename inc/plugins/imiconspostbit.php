c9a5f8a297c00f10c5ebb68efe03017a5e378a2f
Paul Bender <paulbender@live.com>
2021-06-25 12:14:06 -0400

    Bump to version v1.3

--- inc/plugins/imiconspostbit.php
+++ inc/plugins/imiconspostbit.php
@@ -92,7 +92,7 @@ function imiconspostbit_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.2",
+		"version"			=> "1.3",
 		"codename"			=> "imiconspostbit",
 		"compatibility"		=> "18*"
 	);
